4a0a4ead3ccab626ffa2fe74ecf8bd62de701363
sahed36 <mdsahed.ict@gmail.com>
2024-11-18 11:43:33 +0600

    Add files via upload

---
+++ Chapter 1/Ex1.java
@@ -0,0 +1,9 @@
+public class Ex1 {
+   //Main Method
+   public static void main(String[] args) {
+     /** Display output */
+     System.out.println("Welcome to Java");
+     System.out.println("Welcome to Computer Science");
+     System.out.println("Programming is Fun");
+   } 
+}
---
+++ Chapter 1/Ex4.java
@@ -0,0 +1,10 @@
+public class Ex4 {
+    public static void main(String[] args) {
+        System.out.println("a     a^2   a^3");
+
+        for(int i = 1; i <=4; i ++) {
+        System.out.printf("%d      %d     %d%n",i,i*i,i*i*i);
+        
+        }
+    }
+}
